e80350b4e0df1c62c77d7195e3e092bb828d9966
Palina <polina.sofina.20@mail.ru>
2024-08-19 18:47:28 +0500

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,26 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    print(matrix)
+    return matrix
+
+get_matrix(2,2,10)
+get_matrix(3,5,42)
+get_matrix(4,2,13)
+
+
+# n = int(input('Количество строк: '))
+# m = int(input('Количество столбцов: '))
+# value = int(input('Значение матрицы: '))
+#
+# get_matrix(n, m, value)
+
+
+
+
+
+
+
